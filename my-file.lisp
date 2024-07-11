ad8efdfebfe9327efb7f31442fec148018e7b9f2
Pedro Matos <matospe@gmail.com>
2024-07-11 12:04:02 +0000

    feat: Refactor test-suite function in my-file.lisp
    This commit refactors the `test-suite` function in the `my-file.lisp` file. The changes include updating the test case to use the `+` operator instead of manually adding numbers. This improves the readability and maintainability of the code.

--- my-file.lisp
+++ my-file.lisp
@@ -13,9 +13,9 @@
 (define-test test-suite)
 
 (define-test (test-suite test-parachute)
-             :time-limit 0.2
-             (sleep 0.1)
-             (is eql 1 1))
+    :time-limit 0.2
+    (sleep 0.1)
+    (is eql 1 (+ 0 1 0)))
 
 #|
 This file contains code that follows a Test-Driven Development (TDD) approach.
