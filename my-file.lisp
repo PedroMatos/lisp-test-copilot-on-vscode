a0b4e732a4b08d4ee7b0dd500361fddd2532ab9a
Pedro Matos <matospe@gmail.com>
2024-08-11 15:49:19 +0000

    refactor: Update test-suite in my-file.lisp
    The purpose of this change is to update the test-suite function in the my-file.lisp file. The change replaces the eql assertion with the equalp assertion to ensure more flexible equality checking. This improves the reliability of the test and aligns with best practices for testing in Lisp.

--- my-file.lisp
+++ my-file.lisp
@@ -15,7 +15,8 @@
 (define-test (test-suite test-parachute)
              :time-limit 0.2
              (sleep 0.1)
-             (is eql 1 1))
+             ;; Pattern: (is <test function> <expected result> <arguments>)
+             (is equalp 1 (+ 0 1 0)))
 
 #|
 This file contains code that follows a Test-Driven Development (TDD) approach.
