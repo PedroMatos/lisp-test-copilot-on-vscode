f640c4de562ccd18b024d6fcd3acbcd8c61cf9b8
Pedro Matos <matospe@gmail.com>
2024-09-18 15:25:40 +0000

    Rehearsal: Implement graph data structure and Dijkstra's algorithm for shortest path
    This commit adds the implementation of a simple graph data structure and the Dijkstra's algorithm for finding the shortest path in the graph. The graph is represented as a list of nodes and arrows, where nodes are Lisp symbols and arrows are structures with source, target, and weight. The Dijkstra's algorithm is implemented according to the pseudocode from Wikipedia with some modifications. The algorithm returns the distance from the source node to each node in the graph and the previous node in the shortest path from the source node to each node. Additionally, this commit includes tests for the arrow structure, graph structure, Dijkstra's algorithm, and the shortest path algorithm.

--- my-file.lisp
+++ my-file.lisp
@@ -21,5 +21,113 @@
 This file contains code that follows a Test-Driven Development (TDD) approach.
 |#
 
-    
+#|
+Lets implement a simple graph data structure and the shortest path algorithm based on the Dijkstra algorithm.
+
+The graph will be represented as a list of nodes and a list of arrows.
+Nodes shall be represented by Lisp symbols.
+Arrows shall be represented by structure of three elements: the source node, the target node, and the weight of the arrow.
+
+Dijkstra algorithm pseudocode from Wikipedia with some modifications:
+ 1  function Dijkstra(Graph, source):
+ 2     
+ 3      for each node n in Graph.Nodes:
+ 4          distance[n] ← INFINITY
+ 5          previous[n] ← UNDEFINED
+ 6          add n to unvisited-nodes
+ 7      distance[source] ← 0
+ 8     
+ 9      while unvisited-nodes is not empty:
+10          u ← node in unvisited-nodes with minimum distance[u]
+11          remove u from unvisited-nodes
+12         
+13          for each arrow a from u still in unvisited-nodes:
+14              alt ← distance[u] + Graph.Weight(a)  ; Calculate the alternative path distance to neighbor v through u
+15              if alt < distance[target(a)]:
+16                  distance[target(a)] ← alt
+17                  previous[target(a)] ← u
+18
+19      return distance[], previous[]
+
+Function Dijkstra returns 2 values: the distance from the source node to each node in the graph and the previous node in the shortest path from the source node to each node in the graph. Both values are represented as a hash table.
+
+|#
+
+;;; Lets test the arrow structure.
+(define-test (test-suite test-arrow)
+  (let ((arrow (make-arrow 'a 'b 1)))
+    (is eql (arrow-source arrow) 'a)
+    (is eql (arrow-target arrow) 'b)
+    (is eql (arrow-weight arrow) 1)))
+
+;;; Lets implement the arrow structure.
+(defstruct (arrow (:constructor make-arrow (source target weight)))
+  source
+  target
+  weight)
+
+;;; Lets test the graph structure.
+(define-test (test-suite test-graph)
+  (let ((graph (make-graph '(a b c) (list (make-arrow 'a 'b 1) (make-arrow 'b 'c 2) (make-arrow 'c 'a 4)))))
+    (is equalp (graph-nodes graph) '(a b c))
+    (is equalp (graph-arrows graph) (list (make-arrow 'a 'b 1) (make-arrow 'b 'c 2) (make-arrow 'c 'a 4)))))
+
+;;; Lets implement the graph structure.
+(defstruct (graph (:constructor make-graph (nodes arrows)))
+  nodes
+  arrows)
+
+;;; Lets test the Dijkstra algorithm.
+(define-test (test-suite test-dijkstra)
+  ;; Lets mock the graph (a b c) with arrows (a b 1) (b c 2) (a c 4).
+  (let ((graph (make-graph '(a b c) (list (make-arrow 'a 'b 1) (make-arrow 'b 'c 2) (make-arrow 'a 'c 4)))))
+    (multiple-value-bind (distance previous) (dijkstra graph 'a)
+      (is equalp (gethash 'a distance) 0)
+      (is equalp (gethash 'b distance) 1)
+      (is equalp (gethash 'c distance) 3)
+      (is equalp (gethash 'a previous) nil)
+      (is equalp (gethash 'b previous) 'a)
+      (is equalp (gethash 'c previous) 'b))))
+
+;;; Lets implement the Dijkstra algorithm.
+(defun dijkstra (graph source)
+  (let ((distance (make-hash-table :test 'eq))
+        (previous (make-hash-table :test 'eq))
+        (unvisited-nodes (copy-list (graph-nodes graph))))
+    (dolist (node (graph-nodes graph))
+      (setf (gethash node distance) most-positive-fixnum)
+      (setf (gethash node previous) nil))
+    (setf (gethash source distance) 0)
+    (loop while unvisited-nodes do
+          (let ((u (car (sort unvisited-nodes (lambda (a b) (< (gethash a distance) (gethash b distance)))))))
+            (setf unvisited-nodes (remove u unvisited-nodes))
+            (dolist (arrow (graph-arrows graph))
+              (when (and (eql (arrow-source arrow) u) (member (arrow-target arrow) unvisited-nodes))
+                (let ((alt (+ (gethash u distance) (arrow-weight arrow))))
+                  (when (< alt (gethash (arrow-target arrow) distance))
+                    (setf (gethash (arrow-target arrow) distance) alt)
+                    (setf (gethash (arrow-target arrow) previous) u)))))))
+    (values distance previous)))
+
+;;; Lets implement the Shortest Path algorithm using the Dijkstra algorithm. Shortest Path returns 2 values: the shortest path from the source node to the target node and the distance of the shortest path.
+;;; Lets test the Shortest Path algorithm.
+(define-test (test-suite test-shortest-path)
+  ;; Lets mock the graph (a b c) with arrows (a b 1) (b c 2) (a c 4).
+  (let ((graph (make-graph '(a b c) (list (make-arrow 'a 'b 1) (make-arrow 'b 'c 2) (make-arrow 'a 'c 4)))))
+    (multiple-value-bind (path distance) (shortest-path graph 'a 'c)
+      (is equalp '(a b c) path)
+      (is equalp distance 3))))
+
+;;; Lets implement the Shortest Path algorithm.
+(defun shortest-path (graph source target)
+  (multiple-value-bind (distance previous) (dijkstra graph source)
+    (let ((path (list target))
+          (node target))
+      (loop while (gethash node previous)
+            do
+            (setf node (gethash node previous))
+            (push node path))
+      (values path (gethash target distance)))))
+
+;;; Lets run the tests.
 (test '(test-suite))
